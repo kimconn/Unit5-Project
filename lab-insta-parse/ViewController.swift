fb4ab8001e6982d68a101c756363adf127dcc3e5
Charlie <11927517+chieger@users.noreply.github.com>
2022-10-29 21:55:44 -0700

    Initial Commit

---
+++ lab-insta-parse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab-insta-parse
+//
+//  Created by Charlie Hieger on 10/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
